59deb6ea535b26fd000c16883fa6213632a6e147
John James Jacoby <johnjamesjacoby@me.com>
2016-02-25 11:36:09 -0600

    Switch to `show_in_menu`

--- includes/events/user-dashboard.php
+++ includes/events/user-dashboard.php
@@ -18,14 +18,14 @@ defined( 'ABSPATH' ) || exit;
  */
 function wp_event_calendar_add_section( $sections = array() ) {
 
-	// News
+	// Events
 	$sections[] = array(
-		'id'         => 'events',
-		'slug'       => 'events',
-		'url'        => '',
-		'label'      => esc_html__( 'Events', 'wp-user-alerts' ),
-		'visibility' => 'visible',
-		'order'      => 20
+		'id'           => 'events',
+		'slug'         => 'events',
+		'url'          => '',
+		'label'        => esc_html__( 'Events', 'wp-user-alerts' ),
+		'show_in_menu' => true,
+		'order'        => 20
 	);
 
 	// Return sections
