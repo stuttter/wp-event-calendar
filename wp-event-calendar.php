32504f0221b44fcb914a3af0abe2d37ae89b407d
John James Jacoby <johnjamesjacoby@me.com>
2016-03-30 14:41:06 -0500

    Bump to 0.3.0.

--- wp-event-calendar.php
+++ wp-event-calendar.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://wordpress.org/plugins/wp-event-calendar/
  * Author:      John James Jacoby
  * Author URI:  https://jjj.me/
- * Version:     0.2.4
+ * Version:     0.3.0
  * Description: The best way to manage events in WordPress
  * License:     GPL v2 or later
  */
@@ -78,7 +78,7 @@ function wp_event_calendar_get_plugin_url() {
  * @return int
  */
 function wp_event_calendar_get_asset_version() {
-	return 201603280002;
+	return 201603300001;
 }
 
 /**
