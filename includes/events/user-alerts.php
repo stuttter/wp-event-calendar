5b5ad2ab97619c0d6056e9cd2d1b74d80247d190
John James Jacoby <johnjamesjacoby@me.com>
2016-04-02 17:17:02 -0500

    Only filter single post type queries.

--- includes/events/user-alerts.php
+++ includes/events/user-alerts.php
@@ -21,32 +21,33 @@ defined( 'ABSPATH' ) || exit;
 function wp_event_calendar_alerts_meta_query( $r = array() ) {
 
 	// Post type supports events
-	if ( ! empty( $r['post_type'] ) && post_type_supports( $r['post_type'], 'events' ) ) {
-
-		// Last night at midnight
-		$midnight_yesterday = strtotime( 'Midnight yesterday' );
-
-		// Add to meta_query argument
-		$r['meta_query'][] = array(
-			'relation' => 'OR',
-
-			// Starts after midnight yesterday
-			array(
-				'key'     => 'wp_event_calendar_date_time',
-				'value'   => $midnight_yesterday,
-				'type'    => 'DATETIME',
-				'compare' => '>',
-			),
-
-			// Ends after midnight today
-			array(
-				'key'     => 'wp_event_calendar_end_date_time',
-				'value'   => $midnight_yesterday,
-				'type'    => 'DATETIME',
-				'compare' => '>',
-			)
-		);
+	if ( empty( $r['post_type'] ) || is_array( $r['post_type'] ) || ( 'any' === $r['post_type'] ) || ! post_type_supports( $r['post_type'], 'events' ) ) {
+		return $r;
 	}
+
+	// Last night at midnight
+	$midnight_yesterday = strtotime( 'Midnight yesterday' );
+
+	// Add to meta_query argument
+	$r['meta_query'][] = array(
+		'relation' => 'OR',
+
+		// Starts after midnight yesterday
+		array(
+			'key'     => 'wp_event_calendar_date_time',
+			'value'   => $midnight_yesterday,
+			'type'    => 'DATETIME',
+			'compare' => '>',
+		),
+
+		// Ends after midnight today
+		array(
+			'key'     => 'wp_event_calendar_end_date_time',
+			'value'   => $midnight_yesterday,
+			'type'    => 'DATETIME',
+			'compare' => '>',
+		)
+	);
 
 	return $r;
 }
