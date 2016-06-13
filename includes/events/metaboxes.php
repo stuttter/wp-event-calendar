9ef14570f278ec27e068d5558c8c2b01637e1311
Chris Smith <chris@cgsmith.net>
2016-06-13 16:28:59 -0500

    Removing the class conditions that we do not use right now

--- includes/events/metaboxes.php
+++ includes/events/metaboxes.php
@@ -32,45 +32,21 @@ class Walker_Category_Radio extends Walker_Category_Checklist {
 	 * @param int    $id       ID of the current term.
 	 */
 	public function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
-		if ( empty( $args['taxonomy'] ) ) {
-			$taxonomy = 'category';
-		} else {
-			$taxonomy = $args['taxonomy'];
-		}
-
-		if ( $taxonomy == 'category' ) {
-			$name = 'post_category';
-		} else {
-			$name = 'tax_input[' . $taxonomy . ']';
-		}
+		$taxonomy = $args['taxonomy'];
+		$name = 'tax_input[' . $taxonomy . ']';
 
 		$args['popular_cats'] = empty( $args['popular_cats'] ) ? array() : $args['popular_cats'];
 		$class = in_array( $category->term_id, $args['popular_cats'] ) ? ' class="popular-category"' : '';
 
 		$args['selected_cats'] = empty( $args['selected_cats'] ) ? array() : $args['selected_cats'];
 
-		if ( ! empty( $args['list_only'] ) ) {
-			$aria_cheched = 'false';
-			$inner_class = 'category';
+		/** This filter is documented in wp-includes/category-template.php */
+		$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" .
+			'<label class="selectit"><input value="' . $category->name . '" type="radio" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $category->term_id . '"' .
+			checked( in_array( $category->term_id, $args['selected_cats'] ), true, false ) .
+			disabled( empty( $args['disabled'] ), false, false ) . ' /> ' .
+			esc_html( apply_filters( 'the_category', $category->name ) ) . '</label>';
 
-			if ( in_array( $category->term_id, $args['selected_cats'] ) ) {
-				$inner_class .= ' selected';
-				$aria_cheched = 'true';
-			}
-
-			/** This filter is documented in wp-includes/category-template.php */
-			$output .= "\n" . '<li' . $class . '>' .
-				'<div class="' . $inner_class . '" data-term-id=' . $category->term_id .
-				' tabindex="0" role="checkbox" aria-checked="' . $aria_cheched . '">' .
-				esc_html( apply_filters( 'the_category', $category->name ) ) . '</div>';
-		} else {
-			/** This filter is documented in wp-includes/category-template.php */
-			$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" .
-				'<label class="selectit"><input value="' . $category->name . '" type="radio" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $category->term_id . '"' .
-				checked( in_array( $category->term_id, $args['selected_cats'] ), true, false ) .
-				disabled( empty( $args['disabled'] ), false, false ) . ' /> ' .
-				esc_html( apply_filters( 'the_category', $category->name ) ) . '</label>';
-		}
 	}
 }
 
